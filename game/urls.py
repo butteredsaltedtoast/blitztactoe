5686a61d14e066d932411ebfce4854d8a5cd5884
.butteredtoast <emlgproto@gmail.com>
2026-01-18 18:40:50 -0500

    added github and rudimentary viewcount

--- game/urls.py
+++ game/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("game/<str:room_id>/", views.game, name="game"),
     path("api/rooms/", views.list_rooms, name="list_rooms"),
+    path("api/views/", views.get_views, name="get_views"),
 ]
--- game/views.py
+++ game/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from game.consumers import GAMES
 
+VIEW_COUNT = 0
+
+def get_views(request):
+    global VIEW_COUNT
+    VIEW_COUNT += 1
+    return JsonResponse({"views": VIEW_COUNT})
+
 
 def home(request):
     return render(request, "game/home.html")
